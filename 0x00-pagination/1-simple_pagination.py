a8ec90a9ac8b86ed8f20d3beca675c95a540788a
Franklinson <franklinhassey@gmail.com>
2023-12-31 23:11:26 +0000

    data population

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -36,8 +36,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
         dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
@@ -45,4 +45,4 @@ class Server:
         if start_index >= len(dataset):
             return []
 
-        return dataset[start_index:end_index + 1]
+        return dataset[start_index:end_index]
