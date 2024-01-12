08c1413822efd7b13c224b6ba111977204829d70
Franklinson <franklinhassey@gmail.com>
2024-01-12 12:33:39 +0000

    minor edit

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ a simple flask application """
 
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
-    """ Configuring language and timezone"""
+    """Configuring language and timezone"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -21,14 +22,14 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """ Get the best match language """
-    return request.accept_languages.best_match(app.config["en", "fr"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def hello():
-    """return the root page"""
+def index():
+    """ return the root page """
     return render_template('2-index.html')
 
 
 if __name__ == '__main__':
-    app.run(port="5000", host='0.0.0.0', debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
